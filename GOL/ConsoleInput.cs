17a52f80f6cbcf6c32dc8087f4b8bafccc7b347a
Alksnis <deivs.oskars.alksnis@accenture.com>
2019-03-29 15:34:00 +0200

    Add project files.

---
+++ GOL/ConsoleInput.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOL
+{
+    public class ConsoleInput
+    {
+        public static void CreateCells(Board grid)
+        {
+            Console.WriteLine("\nCreate live cells");
+            char confirm;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Enter x (0 to 15):");
+                    int x = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter y (0 to 15):");
+                    int y = Convert.ToInt32(Console.ReadLine());
+                    grid.SetValue(x , y);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("You did an oopsie, try doing it again correctly");
+                }
+                finally
+                {
+                    Console.Clear();
+                    ConsoleOutput.DisplayBoard(grid);
+                    Console.WriteLine("Make another cell? (y/n)");
+                    confirm = Console.ReadKey().KeyChar;
+                    Console.Clear();
+                    ConsoleOutput.DisplayBoard(grid);
+                }
+            } while (confirm == 'y');
+        }
+    }
+}
---
+++ GOL/ConsoleOutput.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOL
+{
+    public static class ConsoleOutput
+    {
+        public static void ConfigureConsole()
+        {
+            Console.SetWindowSize(ConfigSettings.Width * 8 + 4, ConfigSettings.Height * 2 + 10);
+            Console.SetBufferSize(ConfigSettings.Width * 8 + 4, ConfigSettings.Height * 2 + 10);
+        }
+
+        public static void DisplayBoard(Board grid)
+        {
+            var displayString = new StringBuilder();
+            for (int y = 0; y < ConfigSettings.Height; y++)
+            {
+                for (int x = 0; x < ConfigSettings.Width; x++)
+                {
+                    char c = grid.GetValue(x, y) ? ConfigSettings.FullBlock : ConfigSettings.EmptyBlock;
+                    // 2 appends because that will make a nice square
+                    displayString.Append(c);
+                    displayString.Append(c);
+                }
+                displayString.Append("\n");
+            }
+            Console.Clear();
+            Console.SetCursorPosition(0, 0);
+            Console.Write(displayString.ToString());
+        }
+    }
+}
